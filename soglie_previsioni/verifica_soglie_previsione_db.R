6feced8644a91be4ebf37aad29448f0125ca599d
Paolo Saudin <paolosaudin@gmail.com>
2013-04-10 14:07:22 +0200

    added cancellinova.txt query

--- soglie_previsioni/verifica_soglie_previsione_db.R
+++ soglie_previsioni/verifica_soglie_previsione_db.R
@@ -71,47 +71,53 @@ PG.DisconnectAll()
 #- connect to database server
 DBH <- PG.Connect(CONF$Database)
 
+#- ----------------------------------------------------------------------
 #- get bulletin ID - year + julian day
 jd <- as.numeric(format(Sys.Date(), "%Y%j"))
 
+#- ----------------------------------------------------------------------
 #-- build the precipitaz.txt query
 query <- paste("
-SELECT  'A' as zona,
-    max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
-    max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
-    max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
-    max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
-    max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
+    SELECT
+        'A' as zona,
+        max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
+        max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
+        max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
+        max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
+        max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='A'
     GROUP BY 1
     UNION ALL
-    SELECT  'B' as zona,
-    max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
-    max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
-    max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
-    max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
-    max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
+    SELECT
+        'B' as zona,
+        max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
+        max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
+        max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
+        max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
+        max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='B'
     GROUP BY 1
     UNION ALL
-    SELECT  'C' as zona,
-    max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
-    max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
-    max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
-    max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
-    max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
+    SELECT
+        'C' as zona,
+        max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
+        max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
+        max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
+        max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
+        max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='C'
     GROUP BY 1
     UNION ALL
-    SELECT  'D' as zona,
-    max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
-    max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
-    max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
-    max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
-    max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
+    SELECT
+        'D' as zona,
+        max(CASE WHEN id_h12=12 THEN med END)    AS pmed0,
+        max(CASE WHEN id_h12=24 THEN med END)    AS pmed12,
+        max(CASE WHEN id_h12=36 THEN med END)    AS pmed24,
+        max(CASE WHEN id_h12=36 THEN max_12 END) AS pmax12h,
+        max(CASE WHEN id_h12=36 THEN max_24 END) AS pmax24h
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='D'
     GROUP BY 1", sep="")
@@ -120,53 +126,58 @@ dbdata <- PG.ExecuteQuery(DBH, query)
 dbdata <- dbdata[,-1]
 if (DEBUG) dataframe_report(dbdata)
 
+#- ----------------------------------------------------------------------
 #- export file - work dir + ....
 file <- paste(CONF$Options$base_path, "input","precipitaz.txt", sep="/")
 dataframe_dump(dbdata, file)
 
-
+#- ----------------------------------------------------------------------
 #-- build the zterm_qneve.txt
 query <- paste("
-SELECT  'A' as zona,
-    max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
-    max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
-    max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
-    max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
-    max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
-    max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
+    SELECT
+        'A' as zona,
+        max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
+        max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
+        max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
+        max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
+        max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
+        max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='A'
     GROUP BY 1
     UNION ALL
-    SELECT  'B' as zona,
-    max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
-    max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
-    max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
-    max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
-    max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
-    max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
+    SELECT
+        'B' as zona,
+        max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
+        max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
+        max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
+        max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
+        max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
+        max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='B'
     GROUP BY 1
     UNION ALL
-    SELECT  'C' as zona,
-    max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
-    max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
-    max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
-    max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
-    max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
-    max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
+    SELECT
+        'C' as zona,
+        max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
+        max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
+        max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
+        max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
+        max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
+        max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='C'
     GROUP BY 1
     UNION ALL
-    SELECT  'D' as zona,
-    max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
-    max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
-    max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
-    max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
-    max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
-    max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
+    SELECT
+        'D' as zona,
+        max(CASE WHEN id_h12=12 THEN zero_termico END) AS zt_0,
+        max(CASE WHEN id_h12=24 THEN zero_termico END) AS zt_12,
+        max(CASE WHEN id_h12=36 THEN zero_termico END) AS zt_24,
+        max(CASE WHEN id_h12=12 THEN quota_neve   END) AS qn_0,
+        max(CASE WHEN id_h12=24 THEN quota_neve   END) AS qn_12,
+        max(CASE WHEN id_h12=36 THEN quota_neve   END) AS qn_24
     FROM bulletins_meteo.bullettin_vigilance_data_h12
     WHERE bl_id = '",jd,"' AND zona='D'
     GROUP BY 1", sep="")
@@ -175,21 +186,44 @@ dbdata <- PG.ExecuteQuery(DBH, query)
 dbdata <- dbdata[,-1]
 if (DEBUG) dataframe_report(dbdata)
 
+#- ----------------------------------------------------------------------
 #- export file - work dir + ....
 file <- paste(CONF$Options$base_path, "input","zterm_qneve.txt", sep="/")
 dataframe_dump(dbdata, file)
 
+#- ----------------------------------------------------------------------
+#-- build the cancellinova.txt
+query <- paste("
+    SELECT
+        m.fulldate AS data,
+        case when (tables_qca.tbl_zona_a.id_4_cod <= 2 ) then round(cast( tables_qca.tbl_zona_a.id_4  AS numeric), 1) end AS zonaa,
+        case when (tables_qca.tbl_zona_b.id_4_cod <= 2 ) then round(cast( tables_qca.tbl_zona_b.id_4  AS numeric), 1) end AS zonab,
+        case when (tables_qca.tbl_zona_c.id_4_cod <= 2 ) then round(cast( tables_qca.tbl_zona_c.id_4  AS numeric), 1) end AS zonac,
+        case when (tables_qca.tbl_zona_d.id_4_cod <= 2 ) then round(cast( tables_qca.tbl_zona_d.id_4  AS numeric), 1) end AS zonad
+    FROM _master m
+        LEFT JOIN tables_qca.tbl_zona_a USING(fulldate)
+        LEFT JOIN tables_qca.tbl_zona_b USING(fulldate)
+        LEFT JOIN tables_qca.tbl_zona_c USING(fulldate)
+        LEFT JOIN tables_qca.tbl_zona_d USING(fulldate)
+    WHERE m.fulldate >= date_trunc('hour', TIMEZONE('UTC',CURRENT_TIMESTAMP) - interval '3 hour')
+    ORDER BY data LIMIT 4", sep="")
+dbdata <- PG.ExecuteQuery(DBH, query)
+#- remove first column
+dbdata <- dbdata[,-1]
+if (DEBUG) dataframe_report(dbdata)
 
+#- ----------------------------------------------------------------------
+#- export file - work dir + ....
+file <- paste(CONF$Options$base_path, "input","cancellinova.txt", sep="/")
+dataframe_dump(dbdata, file)
 
 #- ----------------------------------------------------------------------
 #- disconnect from database server
 PG.Disconnect(DBH)
 
 #- ----------------------------------------------------------------------
-#- denise -
-
+#- run denise script
 source(external_program)
-
 
 #- ----------------------------------------------------------------------
 #- end -
